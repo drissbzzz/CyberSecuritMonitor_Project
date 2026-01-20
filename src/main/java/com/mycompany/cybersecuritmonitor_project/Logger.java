e5b0a3cc0873afddb0f7a2413fa327fbd0505225
ddrrss <dbe0007@alu.medac.es>
2026-01-20 08:47:52 +0100

    Inicio

---
+++ src/main/java/com/mycompany/cybersecuritmonitor_project/Logger.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cybersecuritmonitor_project;
+
+/**
+ *
+ * @author alumno
+ */
+public class CyberSecuritMonitor_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
