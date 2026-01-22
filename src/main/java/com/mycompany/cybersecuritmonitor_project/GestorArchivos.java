74b6d907b87e75a3110bd9362d2aea5ed187b646
ddrrss <dbe0007@alu.medac.es>
2026-01-22 14:35:25 +0100

    Solucionado problema de lógica (Módulo 1): solo falta aplicar al hilo

--- src/main/java/com/mycompany/cybersecuritmonitor_project/GestorArchivos.java
+++ src/main/java/com/mycompany/cybersecuritmonitor_project/GestorArchivos.java
@@ -14,6 +14,14 @@ import java.util.ArrayList;
 public class GestorArchivos {
 
     String ruta;
+
+    public ArrayList<Archivo> getPrimerGuardado() {
+        return primerGuardado;
+    }
+
+    public ArrayList<Archivo> getComparativa() {
+        return comparativa;
+    }
     ArrayList<Archivo> primerGuardado = new ArrayList<Archivo>();
     ArrayList<Archivo> comparativa = new ArrayList<Archivo>();
     Logger logger = new Logger("log_sdas.txt");
@@ -34,22 +42,79 @@ public class GestorArchivos {
         this.comparativa = comparativa;
     }
     
-    public void listarCarpeta(File ruta){
-            for (File fichero : ruta.listFiles()){
-                if (fichero.isDirectory()){
-                    listarCarpeta(fichero);
+    public void crearRegistroCarpeta(File ruta){
+        ArrayList<Archivo> lista = new ArrayList<>();  
+        for (File f : ruta.listFiles()){
+                if (f.isDirectory()){
+                    System.out.println(f.getName()+ " - Carpeta"); 
                 }else{
-                    System.out.println(fichero.getName()+" - Ruta: "+fichero.getPath());
+                    System.out.println("Archivo añadido");
+                    Archivo a = new Archivo(f); 
+                    lista.add(a);
                 }
             }
+        setPrimerGuardado(lista);
     }
-
-    public void crearRegistroCarpeta(String ruta) {
-        ArrayList<Archivo> listaArchivos = new ArrayList<Archivo>();
-        File carpeta = new File(ruta);
-        
-        
-        
+    
+    public void crearComparativa(File ruta){
+        ArrayList<Archivo> lista = new ArrayList<>();  
+        for (File f : ruta.listFiles()){
+                if (f.isDirectory()){
+                    
+                }else{
+                    Archivo a = new Archivo(f); 
+                    lista.add(a);
+                }
+            }
+        setComparativa(lista);
     }
-  
+    public void escanearCarpeta() {
+        if (getPrimerGuardado().size() > 0) {
+            // Recorre cada archivo antiguo
+            for (int i = 0; i < getPrimerGuardado().size(); i++) {
+                boolean coincidencia = false;
+                // Recorre todos los archivos de la comparativa
+                for (int j = 0; j < getComparativa().size(); j++) {
+                    String hashAntiguo = getPrimerGuardado().get(i).getHash();
+                    String nombreAntiguo = getPrimerGuardado().get(i).getNombre();
+                    String hashNuevo = getComparativa().get(j).getHash();
+                    String nombreNuevo = getComparativa().get(j).getNombre();
+                    if (hashAntiguo.equals(hashNuevo)) {
+                        /*System.out.println(nombreAntiguo + " es igual");
+                        logger.escribir("["+getComparativa().get(j).getFechaEscaneo()+"][INTEGRIDAD]"+nombreAntiguo + " es igual");*/
+                        coincidencia = true;
+                    } else if (nombreAntiguo.equals(nombreNuevo)) {
+                        System.out.println(nombreAntiguo + " se modifico");
+                        logger.escribir("["+getComparativa().get(j).getFechaEscaneo()+"][INTEGRIDAD] "+nombreAntiguo + " se modifico");
+                        coincidencia = true;
+                    }
+                }
+                // Después de comparar con todos los archivos nuevos
+                if (!coincidencia) {
+                    System.out.println(getPrimerGuardado().get(i).getNombre() + " se elimino");
+                    logger.escribir("["+getComparativa().get(i).getFechaEscaneo()+"][INTEGRIDAD] "+getPrimerGuardado().get(i).getNombre()+ " se elimino");
+                }
+            }
+            for (int i = 0; i < getComparativa().size(); i++) {
+                boolean nuevo = true;
+                for (int j = 0; j < getPrimerGuardado().size(); j++) {
+                    String hashAntiguo = getPrimerGuardado().get(j).getHash();
+                    String nombreAntiguo = getPrimerGuardado().get(j).getNombre();
+                    String hashNuevo = getComparativa().get(i).getHash();
+                    String nombreNuevo = getComparativa().get(i).getNombre();
+                    if (hashAntiguo.equals(hashNuevo)) {
+                        nuevo = false;
+                    } else if (nombreAntiguo.equals(nombreNuevo)) {
+                        nuevo = false;
+                    }
+                }
+                if (nuevo == true) {
+                    System.out.println(getComparativa().get(i).getNombre() + " se creo");
+                    logger.escribir("["+getComparativa().get(i).getFechaEscaneo()+"][INTEGRIDAD] "+getComparativa().get(i).getNombre()+ " se creo");
+                }
+            }
+        } else {
+            System.out.println("No se encontró un guardado primario para continuar con el escaneo");
+        }
+    }
 }
